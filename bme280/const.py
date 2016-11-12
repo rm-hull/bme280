2e9ed426024540c28b4c891628102818d094e69d
Richard Hull <rm_hull@yahoo.co.uk>
2016-11-12 22:02:28 +0000

    Add deladdr and str methods

--- bme280/const.py
+++ bme280/const.py
@@ -31,10 +31,18 @@ class _const:
     class ConstError(TypeError):
         pass
 
-    def __setattr__(self,name,value):
+    def __setattr__(self, name, value):
         if name in self.__dict__:
             raise self.ConstError("Can't rebind const(%s)" % name)
         self.__dict__[name] = value
+
+    def __delattr__(self, name, value):
+        if name in self.__dict__:
+            raise self.ConstError("Can't unbind const(%s)" % name)
+        self.__dict__[name] = value
+
+    def __str__(self):
+        return str(self.__dict__)
 
 import sys
 
