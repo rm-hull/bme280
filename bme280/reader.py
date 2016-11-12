1069aeac7d520379af7996627f0d38add36b2ace
Richard Hull <rm_hull@yahoo.co.uk>
2016-11-12 22:00:42 +0000

    Missing return statement

--- bme280/reader.py
+++ bme280/reader.py
@@ -36,14 +36,14 @@ class reader(object):
         self._address = address
 
     def unsigned_short(self, register):
-        self._bus.read_word_data(self._address, register) & 0xffff
+        return self._bus.read_word_data(self._address, register) & 0xffff
 
     def signed_short(self, register):
         word = self.unsigned_short(register)
         return word if word < 0x8000 else word - 0x10000
 
     def unsigned_byte(self, register):
-        self._bus.read_byte_data(self._address, register) & 0xff
+        return self._bus.read_byte_data(self._address, register) & 0xff
 
     def signed_byte(self, register):
         byte = self.unsigned_byte(register) & 0xff
