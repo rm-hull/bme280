730fd1991056e5b10d9775d17ddd60641518d63f
Richard Hull <rm_hull@yahoo.co.uk>
2016-11-15 21:05:53 +0000

    Set version & dependencies

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@
 import os
 from setuptools import setup
 
+import bme280
+
 README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
 setup(
-    name = "rpi-bme280",
-    version = "0.1.0",
+    name = "RPi.bme280",
+    version = bme280.__version__,
     author = "Richard Hull",
     author_email = "richard.hull@destructuring-bind.org",
     description = "A library to drive a Bosch BME280 tempperature, humidity, pressure sensor over I2C",
@@ -15,8 +17,9 @@ setup(
     license = "MIT",
     keywords = ["raspberry pi", "rpi", "bosch", "BME280", "i2c", "temperature", "humidity", "pressure"],
     url = "https://github.com/rm-hull/bme280",
-    download_url = "https://github.com/rm-hull/bme280/tarball/0.1.0",
+    download_url = "https://github.com/rm-hull/bme280/tarball/" + bme280.__version__,
     packages = ['bme280'],
+    install_requires = ["pillow", "smbus2"],
     classifiers = [
         "License :: OSI Approved :: MIT License",
         "Development Status :: 4 - Beta",
