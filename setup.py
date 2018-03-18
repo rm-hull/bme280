5611400ffa18f4a2b4b0feb4fee78eca1e260c1e
Richard Hull <rm_hull@yahoo.co.uk>
2018-03-18 18:17:43 +0000

    Update classifiers

--- setup.py
+++ setup.py
@@ -46,6 +46,10 @@ setup(
         "Topic :: Education",
         "Topic :: System :: Hardware",
         "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 3"
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6"
     ]
 )
