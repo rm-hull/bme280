07a01113340550b2b5ae49e5dea0a4dc38f25769
Paul Fisher <pfish@google.com>
2019-10-16 21:56:25 -0400

    Also clean up timezone imports on test_bme280.py.

--- tests/test_bme280.py
+++ tests/test_bme280.py
@@ -8,8 +8,9 @@ try:
 except ImportError:
     from mock import Mock, MagicMock  # noqa: F401
 
-from datetime import datetime, timezone
+from datetime import datetime
 import bme280
+import pytz
 
 smbus = Mock(unsafe=True)
 
@@ -101,7 +102,7 @@ def test_compensated_readings_repr():
     raw = bme280.uncompensated_readings(block)
     reading = bme280.compensated_readings(raw, compensation_params)
     reading.id = "55fea298-5a5d-4873-a46d-b631c8748100"
-    reading.timestamp = datetime(2018, 3, 18, 19, 26, 14, 206233, tzinfo=timezone.utc)
+    reading.timestamp = datetime(2018, 3, 18, 19, 26, 14, 206233, tzinfo=pytz.UTC)
     assert repr(reading) == "compensated_reading(id=55fea298-5a5d-4873-a46d-b631c8748100, timestamp=2018-03-18 19:26:14.206233UTC, temp=0.003 °C, pressure=8758647.58 hPa, humidity=0.05 % rH)"
 
 
@@ -110,5 +111,5 @@ def test_compensated_readings_repr_zero_millis():
     raw = bme280.uncompensated_readings(block)
     reading = bme280.compensated_readings(raw, compensation_params)
     reading.id = "55fea298-5a5d-4873-a46d-b631c8748100"
-    reading.timestamp = datetime(2018, 3, 18, 19, 26, 14, tzinfo=timezone.utc)
+    reading.timestamp = datetime(2018, 3, 18, 19, 26, 14, tzinfo=pytz.UTC)
     assert repr(reading) == "compensated_reading(id=55fea298-5a5d-4873-a46d-b631c8748100, timestamp=2018-03-18 19:26:14.000000UTC, temp=0.003 °C, pressure=8758647.58 hPa, humidity=0.05 % rH)"
